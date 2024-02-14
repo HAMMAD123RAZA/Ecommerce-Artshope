9404fa40a34df601d4e96624d54acbb1cc0c3332
Hammad Raza <01hammadraza@gmail.com>
2024-02-14 20:23:35 +0500

    first commit

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="flex flex-center items-center justify-center h-screen max-w-full">
+      <SignIn />
+    </div>
+  )
 }
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp />;
+  return(
+  
+    <div className='flex flex-center items-center justify-center h-screen max-w-full'>
+  <SignUp />;
+    </div>
+    )
+
 }
